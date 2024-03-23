9ee14703b2cb3cc9356d9bec875e633458149a56
Dave T <74991341+davethomson2000@users.noreply.github.com>
2024-03-23 15:39:54 +0000

    complete exercise 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (phrase, times) {
+    let repeatedString = ""
+    if (times >= 0) {
+        for (let i = 0; i < times; i++) {
+            repeatedString += phrase
+        }
+    } else {
+        repeatedString = "ERROR"
+    }
+    return repeatedString
 };
 
 // Do not edit below this line
