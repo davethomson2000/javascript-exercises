d3cbc6d73ade180fa6b66278ea901d2ad48c7c1f
Dave T <74991341+davethomson2000@users.noreply.github.com>
2024-03-24 09:00:52 +0000

    complete ex 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(startArray = [], ...removeThese) {
+    //console.log(removeThese)
+    for (const remove of removeThese) {
+        //console.log("remove: " + remove)
+        while (startArray.indexOf(remove) > -1) {
+            let index = startArray.indexOf(remove)
+            //console.log("index: " + index)
+            startArray.splice(index,1)
+        }
+    }
+    return startArray
 };
-
+//console.log(removeFromArray([1,2,3,4],3,2))
 // Do not edit below this line
 module.exports = removeFromArray;
