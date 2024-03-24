97c3fe397e0736a554996812b455cbfcc5b1fa83
Dave T <74991341+davethomson2000@users.noreply.github.com>
2024-03-24 12:08:23 +0000

    complete ex 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,26 @@
-const findTheOldest = function() {
+function compareAge(personA, personB) {
+    return (calculateAge(personA) - calculateAge(personB))
+}
 
+function calculateAge(person) {
+    let age
+    if (person.hasOwnProperty('yearOfDeath')) {
+        age = person.yearOfDeath - person.yearOfBirth    
+    }
+    else {
+        let thisYear = new Date().getFullYear()
+        age = thisYear - person.yearOfBirth
+    }
+
+    return age
+}
+
+const findTheOldest = function(people) {
+    people.sort(compareAge)
+    people.reverse()
+    return people[0]
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
