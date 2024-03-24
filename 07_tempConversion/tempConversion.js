1e9f9a538fa24a1101b904db3e8113107d69daf8
Dave T <74991341+davethomson2000@users.noreply.github.com>
2024-03-24 09:45:37 +0000

    complete ex 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureInFahrenheit) {
+  // x °C ≘ (x × 9/5 + 32) °F
+  return +((temperatureInFahrenheit - 32) * 5 / 9).toFixed(1)  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureInCelsius) {
+  //x °F ≘ (x − 32) × 5 / 9 °C
+  return +((temperatureInCelsius * 9 / 5) + 32).toFixed(1)  
 };
 
 // Do not edit below this line
