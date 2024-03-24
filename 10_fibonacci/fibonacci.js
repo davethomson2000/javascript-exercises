f148a5d60763a0114d404bc552b78378605803b6
Dave T <74991341+davethomson2000@users.noreply.github.com>
2024-03-24 10:58:59 +0000

    complete ex 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function (numberInSeries) {
+    let recentNumberOne = 0
+    let recentNumberTwo = 0
+    let fibonacciNumber
 
+    //due to start of the fib series, decrement by 1 to get correct results
+    numberInSeries = +numberInSeries
+    if (numberInSeries < 0)
+        return "OOPS"
+
+    for (let i = 0; i <= numberInSeries; i++) {
+        if (i === 1)
+            fibonacciNumber = 1
+        else
+            fibonacciNumber = recentNumberOne + recentNumberTwo;
+        recentNumberOne = recentNumberTwo
+        recentNumberTwo = fibonacciNumber
+    }
+    return fibonacciNumber
 };
 
 // Do not edit below this line
