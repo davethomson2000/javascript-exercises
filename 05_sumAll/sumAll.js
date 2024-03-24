1648899aacf2ffb60536ce778dd55e31ad6cb1d3
Dave T <74991341+davethomson2000@users.noreply.github.com>
2024-03-24 09:19:25 +0000

    complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber = 0, lastNumber = 0) {
+    let sumAll = 0
+    if (typeof(firstNumber) != "number" || typeof(lastNumber) != "number") {
+        return "ERROR"
+    }
+    else if (firstNumber < 0 || lastNumber < 0) {
+        return "ERROR"
+    } 
+    else {
+            for (let i = Math.min(firstNumber,lastNumber); i <= Math.max(firstNumber,lastNumber); i++) {
+            sumAll += i
+        }
+    }
+    return sumAll
 };
 
 // Do not edit below this line
