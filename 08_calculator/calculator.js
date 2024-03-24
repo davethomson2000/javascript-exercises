11c269e76af28ee0bc821743b4b9dde785dff222
Dave T <74991341+davethomson2000@users.noreply.github.com>
2024-03-24 10:20:34 +0000

    complete ex 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a = 0, b = 0) {
+	return(a + b)
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return(a - b)
 };
 
-const sum = function() {
-	
+const sum = function(nums = []) {
+	let sum = 0
+  for (const num of nums) {
+    sum += num
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(nums = []) {
+  let sum = 1
+  for (const num of nums) {
+    sum *= num
+  }
+  return sum
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return (a ** b)
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let factorial = 1
+  for (let i = 1; i <= a; i++) {
+    factorial *= i
+  }
+  return factorial
 };
 
 // Do not edit below this line
