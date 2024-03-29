622d01cec8e065c790c013da0fa86ac966a0c79d
Dave T <74991341+davethomson2000@users.noreply.github.com>
2024-03-29 15:34:46 +0000

    gentle clean of ex 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,11 +1,10 @@
 const palindromes = function (word = "") {
     word = word.replace(/[^\w]/g,"").toLowerCase()
-    console.log(word)
     if (word.split("").reverse().join("") === word)
         return true
     else 
         return false
 };
-palindromes("racecar!")
+
 // Do not edit below this line
 module.exports = palindromes;
